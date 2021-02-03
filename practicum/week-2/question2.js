331ed8ded07b2bbcd2fd7b0b04fb3785dae1dd31
apple <apple@appledeMacBook-Pro.local>
2021-02-03 19:52:32 +0800

    qs

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -10,6 +10,8 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
  */
 
  var canGetEqualCookies = function(cookies, extraCookies) {
+         var max=Math.max(cookies);
+         
      
     
  };
